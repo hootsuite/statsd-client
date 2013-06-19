c476f14873e25b139d064c97d588c313b994a285
Jeremy Pierre <jeremy.pierre@hootsuite.com>
2013-06-19 11:10:44 -0700

    Forgot a timing method.

--- src/main/scala/com/hootsuite/statsd/StatsdHandler.scala
+++ src/main/scala/com/hootsuite/statsd/StatsdHandler.scala
@@ -25,4 +25,5 @@ class StatsdHandler(config: Config) {
   def inc(k: String, v: Int = 1): Unit = client.increment(s"${prefix}k", v)
   def dec(k: String, v: Int = 1): Unit = client.decrement(s"${prefix}k", v)
   def gauge(k: String, v: Int): Unit = client.gauge(s"${prefix}k", v)
+  def timer(k: String, v: Int): Unit = client.timing(s"${prefix}k", v)
 }
